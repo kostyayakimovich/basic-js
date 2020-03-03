8f36b3084ecefdb8e98d7db19480820bc0f443da
kostyayakimovich <yakimovichkostya@gmail.com>
2020-03-03 11:14:54 +0300

    feat: add solution dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,17 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)) return false;
+
+  let clearArr = members.filter(value => typeof value === "string");
+  if (clearArr.length < 1) return false;
+
+  let masLetters = clearArr.map(function letter(value) {
+    return value
+      .trim()
+      .charAt(0)
+      .toUpperCase();
+  }, "");
+
+  let mas = masLetters.sort().join("");
+
+  return mas;
 };
